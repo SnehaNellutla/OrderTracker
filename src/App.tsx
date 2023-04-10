879d1fe1b1f097fc6dc39050cddd65ab950414ae
agnuvolo <abhiji.gund@nuvolo.com>
2023-04-10 22:09:27 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { OO } from './OO';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { OO } from "./OO";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 function App() {
   return (
-   
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Orders
+          </Typography>
 
-   <OO/> 
-     
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              // onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <OO />
     </div>
   );
 }
